4b5f6c351dd9539e87bdcc58d2a69784dbd7e067
cihan <cihanisikal@outlook.com>
2024-04-17 12:51:14 +0300

    code is working with plots now ln12

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pandas as pd
-import ydata_profiling as pdpf
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
+
+warnings.filterwarnings("ignore", message=".*invalid escape sequence.*", category=SyntaxWarning)
+warnings.filterwarnings("ignore", message=".*use_inf_as_na option is deprecated.*", category=FutureWarning)
 
 
 def main():
+    # Set the use_inf_as_na option to False
+    pd.set_option('mode.use_inf_as_na', False)
+
     # Define the new path to your dataset
     new_dataset_path = 'D:/Projects/predictive_maintenance/dataset_1.csv'
 
     # Read the dataset from the new path
     df = pd.read_csv(new_dataset_path)
+
+    # Convert inf values to NaN
+    df.replace([np.inf, -np.inf], np.nan, inplace=True)
 
     # total number of rows and columns
     print(f"Number of rows: {df.shape[0]}, Number of columns: {df.shape[1]}")
@@ -28,6 +37,26 @@ def main():
     df.drop(['Product ID', 'UDI'], axis=1, inplace=True)
 
     df.head(2)
+
+    df.info()
+
+    # overall descriptive information on numerical attributes
+    df_numeric = df.select_dtypes(include=[np.number])
+    df_numeric.describe().transpose()
+
+    # overall descriptive information on categorical attributes
+    df_categorical = df.select_dtypes(include=[np.object_])
+    df_categorical.describe().transpose()
+
+    fig, ax = plt.subplots(3, 4, figsize=(25, 20))
+
+    for i, col in enumerate(df.columns):
+        sns.histplot(df[col], ax=ax[i // 4][i % 4])
+
+    plt.show()
+
+    # Reset the use_inf_as_na option to its default value
+    pd.reset_option('mode.use_inf_as_na')
 
 
 if __name__ == "__main__":
