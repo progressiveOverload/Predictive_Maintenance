c337f5d7ac879f4c46c84ef04bad1d38d0acab82
cihan <cihanisikal@outlook.com>
2024-04-17 12:41:49 +0300

    code is working now

--- main.py
+++ main.py
@@ -4,25 +4,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Define the new path to your dataset
-new_dataset_path = 'D:/Projects/predictive_maintenance/dataset_1.csv'
 
-# Read the dataset from the new path
-df = pd.read_csv(new_dataset_path)
+def main():
+    # Define the new path to your dataset
+    new_dataset_path = 'D:/Projects/predictive_maintenance/dataset_1.csv'
 
-# total number of rows and columns
-print(f"Number of rows: {df.shape[0]}, Number of columns: {df.shape[1]}")
+    # Read the dataset from the new path
+    df = pd.read_csv(new_dataset_path)
 
-df.head(3)
+    # total number of rows and columns
+    print(f"Number of rows: {df.shape[0]}, Number of columns: {df.shape[1]}")
 
-# rename dataset columns
-df.rename(columns = {'Air temperature [K]':'Air temperature',
-                     'Process temperature [K]':'Process temperature',
-                     'Rotational speed [rpm]':'Rotational speed',
-                     'Torque [Nm]':'Torque',
-                     'Tool wear [min]':'Tool wear'},
-          inplace = True)
+    df.head(3)
 
-df.drop(['Product ID', 'UDI'], axis=1, inplace = True)
+    # rename dataset columns
+    df.rename(columns={'Air temperature [K]': 'Air temperature',
+                       'Process temperature [K]': 'Process temperature',
+                       'Rotational speed [rpm]': 'Rotational speed',
+                       'Torque [Nm]': 'Torque',
+                       'Tool wear [min]': 'Tool wear'},
+              inplace=True)
 
-df.head(2)
+    df.drop(['Product ID', 'UDI'], axis=1, inplace=True)
+
+    df.head(2)
+
+
+if __name__ == "__main__":
+    main()
